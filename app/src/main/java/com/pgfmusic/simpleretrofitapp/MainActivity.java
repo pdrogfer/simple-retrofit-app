c87c2d55af604ca3e94e2847a7fb36d6e8f6544c
pdrogfer <pedro.gf527@gmail.com>
2016-11-16 15:48:09 +0100

    huge refactoring and update

--- app/src/main/java/com/pgfmusic/simpleretrofitapp/MainActivity.java
+++ app/src/main/java/com/pgfmusic/simpleretrofitapp/MainActivity.java
@@ -2,69 +2,70 @@ package com.pgfmusic.simpleretrofitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.GsonConverterFactory;
-import retrofit.Response;
-import retrofit.Retrofit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /*
  * Esta app muestra un uso simple de Retrofit para extraer datos de un servidor(que provee json)
  * en formato array<Objeto>, en este caso customers (de la base de datos 'tnb')
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
 
-    TextView tvResult;
+    Button btnSearch;
+    EditText searchInput;
+    TextView textViewResult;
+    private MovieService movieService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvResult = (TextView) findViewById(R.id.tv_result);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        searchInput = (EditText) findViewById(R.id.editText);
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
+        btnSearch.setOnClickListener(this);
 
-        // all this should be in another class, don't put logic in Activities, just GUI staff,
-        // a cleaner approach for better testing
-        final Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://pgfmusic.com/php_course/")
+        Retrofit retrofitInstance = new Retrofit.Builder()
+                .baseUrl("http://www.omdbapi.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        CustomerService customerService = retrofit.create(CustomerService.class);
-        final Call<List<Customer>> customers = customerService.getCustomer();
-        customers.enqueue(new Callback<List<Customer>>() {
+        movieService = retrofitInstance.create(MovieService.class);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        final Call<List<Movie>> movieCall = movieService.searchMovie(searchInput.getText().toString());
+        movieCall.enqueue(new Callback<List<Movie>>() {
             @Override
-            public void onResponse(Response<List<Customer>> response) {
-                String status = response.message();
-                String result = response.body().toString();
-                tvResult.setText(result);
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+                Log.i(TAG, "onResponse: " + response.body().);
+                for (int i = 0; i < response.body().size(); i++) {
+                    Movie tempMovie = response.body().get(i);
+                    Log.i(TAG, "onResponse: " + tempMovie.getTitle() + ", " + tempMovie.getActors());
+
+                }
             }
 
             @Override
-            public void onFailure(Throwable t) {
-                tvResult.setText(t.getMessage());
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+                Log.i(TAG, "onFailure: " + t.toString());
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
 }
